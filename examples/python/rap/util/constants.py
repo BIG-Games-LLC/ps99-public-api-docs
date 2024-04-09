ed5721673abf184ba67a54e811fca17b72d7ad20
nomnom <93007593+noemtdev@users.noreply.github.com>
2024-04-09 21:47:47 +0200

    added a python example for rap parsing using a class, including how an example bot could look like

---
+++ examples/python/rap/util/constants.py
@@ -0,0 +1,66 @@
+# https://github.com/noemtdev
+
+"""
+POTION CONSTANTS
+"""
+
+SPECIAL_POTIONS = {"Huge": "Huge Potion", "The Cocktail": "The Cocktail"}
+POTION_NAMES = {
+    "Lucky": "Lucky Eggs"
+}
+
+"""
+ENCHANT CONSTANTS
+"""
+
+SPECIAL_ENCHANTS = {
+    "Super Magnet": "Super Magnet",
+    "Boss Chest Mimic": "Boss Chest Mimic",
+    "Lightning Orb": "Lightning Orb",
+    "Magic Orb": "Magic Orb",
+    "Large Taps": "Large Taps",
+    "Diamond Chest Mimic": "Diamond Chest Mimic",
+    "Party Time": "Party Time",
+    "Exotic Pet": "Exotic Pet",
+    "Happy Pets": "Happy Pets",
+    "Chest Breaker": "Chest Breaker",
+    "Starfall": "Starfall",
+    "Fireworks": "Fireworks",
+    "Lightning": "Lightning",
+    "Midas Touch": "Midas Touch",
+    "Super Shiny Hunter": "Super Shiny Hunter",
+    "Tap Teamwork": "Tap Teamwork",
+    "Huge Hunter": "Huge Hunter",
+    "Fruity": "Fruity",
+    "Lucky Block": "Lucky Block",
+    "Fortune": "Fortune",
+    "Explosive": "Explosive",
+    "Massive Comet": "Massive Comet",
+    "Chest Mimic": "Chest Mimic",
+    "Boss Lucky Block": "Boss Lucky Block",
+    "Shiny Hunter": "Shiny Hunter",
+    "Super Lightning": "Super Lightning",
+} # I wrote them as a dictionary just in case furture updates add obscure naming in the api
+
+ROMAN_NUMERALS = {
+    1: "I",
+    2: "II",
+    3: "III",
+    4: "IV",
+    5: "V",
+    6: "VI",
+    7: "VII",
+    8: "VIII",
+    9: "IX",
+    10: "X",
+    11: "XI",
+    12: "XII",
+    13: "XIII",
+    14: "XIV",
+    15: "XV",
+    16: "XVI",
+    17: "XVII",
+    18: "XVIII",
+    19: "XIX",
+    20: "XX"
+} # added extra ones to be safe
